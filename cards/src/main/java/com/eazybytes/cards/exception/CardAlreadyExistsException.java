8be1960dd1d997c363d63a1e04b571fb4bdf74d1
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-06-09 22:02:27 +0530

    feat(cards): add CardsAlreadyExistsException

--- cards/src/main/java/com/eazybytes/cards/exception/CardAlreadyExistsException.java
+++ cards/src/main/java/com/eazybytes/cards/exception/CardAlreadyExistsException.java
@@ -4,7 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class CardAlreadyExistsException extends RuntimeException {
+public class CardAlreadyExistsException extends RuntimeException
+{
 
     public CardAlreadyExistsException(String message){
         super(message);
