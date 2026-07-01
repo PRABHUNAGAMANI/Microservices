5c70ddf38e8c109c85fba61b7a45cad07044896e
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-07-01 11:18:20 +0530

    FEAT(loans): add Loans Main Method

---
+++ loans/src/main/java/com/easybytes/loans/LoansApplication.java
@@ -0,0 +1,46 @@
+package com.easybytes.loans;
+
+import com.easybytes.loans.dto.LoansContactInfoDto;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@SpringBootApplication
+/*@ComponentScans({ @ComponentScan("com.eazybytes.loans.controller") })
+@EnableJpaRepositories("com.eazybytes.loans.repository")
+@EntityScan("com.eazybytes.loans.model")*/
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
+@EnableConfigurationProperties(value = {LoansContactInfoDto.class})
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Accounts microservice REST API Documentation",
+				description = "EazyBank Accounts microservice REST API  Documentation",
+				version = "v1",
+				contact = @Contact(
+						name = "PRABHU",
+						email = "prabhu.nagamani98@gmail.com",
+						url = "https://github.com/BL-Prabhu/javafellowship"
+				),
+				license = @License(
+						name = "Apache 2.0",
+						url = "https://github.com/BL-Prabhu/javafellowship"
+				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "EazyBank Accounts microservice REST API  Documentation",
+				url = "https://github.com/BL-Prabhu/javafellowship"
+		)
+)
+public class LoansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoansApplication.class, args);
+	}
+
+}
