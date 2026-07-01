79a5bfbcf033d1264c1fbd298e154ec442406073
PRABHUNAGAMANI <prabhu.nagamani98@gmail.com>
2026-07-01 10:12:26 +0530

    FEAT(cards): add audit-aware implementation

--- cards/src/main/java/com/eazybytes/cards/audit/AuditAwareImpl.java
+++ cards/src/main/java/com/eazybytes/cards/audit/AuditAwareImpl.java
@@ -6,7 +6,8 @@ import org.springframework.stereotype.Component;
 import java.util.Optional;
 
 @Component("auditAwareImpl")
-public class AuditAwareImpl implements AuditorAware<String> {
+public class AuditAwareImpl implements AuditorAware<String>
+{
 
     /**
      * Returns the current auditor of the application.
